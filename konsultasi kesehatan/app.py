05c0a8062216f01d353373b0d1d8bf6dfd440169
anifithraini43 <anifithraini43@guru.sma.belajar.id>
2025-07-24 11:50:57 +0700

    Update app.py

--- konsultasi kesehatan/app.py
+++ konsultasi kesehatan/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
-import os
+#import os
 
 # ==============================================================================
 # PENGATURAN MODEL
